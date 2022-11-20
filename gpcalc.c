gfxprim/gpcalc

commit 34b2bc1711ff4bcb1058d75c46d71c0d8a57f313

Make function context aware
If there is an expression that does not end with operation appending a
function does to produce a valid result.

i.e. if we have typed 42 then type sqrt( appending would produce 42sqrt(
which is not a valid expression.

This commit changes the logic so that the function inserted at the start
of the buffer when the resulting expression wouldn't be correct, i.e.
pressing sqrt( when 42 was in the buffer would produce sqrt(42 instead.

Signed-off-by: Cyril Hrubis <metan@ucw.cz>

gpcalc.c
@@ -112,6 +112,44 @@ int edit_event(gp_widget_event *ev)
 	return do_eq(ev);
 }
 
+static char last_chr(const char *str)
+{
+	char prev = 0;
+
+	while (*str)
+		prev = *(str++);
+
+	return prev;
+}
+
+static int is_func(const char *str)
+{
+	return last_chr(str) == '(';
+}
+
+static int is_op(int ch)
+{
+	switch (ch) {
+	case '*':
+	case '+':
+	case '-':
+	case '/':
+	case '^':
+		return 1;
+	default:
+		return 0;
+	}
+}
+
+static int op_before_cur_pos(gp_widget *tbox)
+{
+	gp_utf8_pos cur_pos = gp_widget_tbox_cursor_get(tbox);
+
+	int ch = gp_utf8_pos_prev(tbox->tbox->buf, &cur_pos);
+
+	return is_op(ch);
+}
+
 int do_append(gp_widget_event *ev)
 {
 	if (ev->type != GP_WIDGET_EVENT_WIDGET)
@@ -125,7 +163,11 @@ int do_append(gp_widget_event *ev)
 	if (!strcmp(label, "\u00f7"))
 		label = "/";
 
-	gp_widget_tbox_ins(edit, 0, GP_SEEK_CUR, label);
+	int ins_whence = GP_SEEK_CUR;
+	if (is_func(label) && !op_before_cur_pos(edit))
+		ins_whence = GP_SEEK_SET;
+
+	gp_widget_tbox_ins(edit, 0, ins_whence, label);
 
 	return 1;
 }

commit a4c327eb129b3b54326f8f49531dec2a79ec973a

We have to append after function too.
Fixes: 34b2bc1711ff ("Make function context aware")
Signed-off-by: Cyril Hrubis <metan@ucw.cz>

gpcalc.c
@@ -127,7 +127,7 @@ static int is_func(const char *str)
 	return last_chr(str) == '(';
 }
 
-static int is_op(int ch)
+static int is_op_fn(int ch)
 {
 	switch (ch) {
 	case '*':
@@ -135,19 +135,20 @@ static int is_op(int ch)
 	case '-':
 	case '/':
 	case '^':
+	case '(':
 		return 1;
 	default:
 		return 0;
 	}
 }
 
-static int op_before_cur_pos(gp_widget *tbox)
+static int op_fn_before_cur_pos(gp_widget *tbox)
 {
 	gp_utf8_pos cur_pos = gp_widget_tbox_cursor_get(tbox);
 
 	int ch = gp_utf8_pos_prev(tbox->tbox->buf, &cur_pos);
 
-	return is_op(ch);
+	return is_op_fn(ch);
 }
 
 int do_append(gp_widget_event *ev)
@@ -164,7 +165,7 @@ int do_append(gp_widget_event *ev)
 		label = "/";
 
 	int ins_whence = GP_SEEK_CUR;
-	if (is_func(label) && !op_before_cur_pos(edit))
+	if (is_func(label) && !op_fn_before_cur_pos(edit))
 		ins_whence = GP_SEEK_SET;
 
 	gp_widget_tbox_ins(edit, 0, ins_whence, label);